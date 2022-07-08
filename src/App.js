7631cf76040cca40e549f86bc8726fcb7505eb0e
ryanlin <ruilin@DESKTOP-1TA1DU7.localdomain>
2022-07-08 19:32:31 -0400

    Added file picker

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from 'react';
 import './App.css';
 import SongUploader from './components/SongUploader'
 import SongVisuals from './components/SongVisuals'
+import { io } from 'socket.io-client';
 
+const socket = io('http://localhost:8081');
+socket.on("connect", () => {
+  console.log(socket.id);
+});
 
 function App() {
   const [songFile, setSongFile] = React.useState(null);
   const [songMood, setSongMood] = React.useState("Happy");
 
-
-  //setSongMood("Happy")
-
   return (
     <div className="App">
-      {/* <SongUploader /> */}
+      <SongUploader/>
       <button
         onClick={() => {
           setSongMood(songMood === "Happy" ? "Sad" : "Happy")
--- src/components/SongUploader.js
+++ src/components/SongUploader.js
@@ -1,8 +1,35 @@
 import React from 'react'
 
+import io from 'socket.io-client'
+
 function SongUploader() {
+  const [selectedFile, setSelectedFile] = React.useState(null);
+  const [fileSelected, setFileSelected] = React.useState(false);
+  
+  const changeHandler = (e) => {
+    setFileSelected(true);
+    setSelectedFile(e.target.files[0]);
+
+    console.log(e.target.files[0].name);
+  }
+
+  const onClickSubmit = (e) => {
+    //empty
+  }
+
   return(
-    <p>Upload a File</p>
+    <div>
+      <input
+        type="file"
+        name="file"
+        onChange={changeHandler}
+      />
+      <button
+        onClick={onClickSubmit}
+      >Upload</button>
+    </div>
+    
+
   )
 }
 
